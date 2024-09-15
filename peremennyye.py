ac11b8bf6c272479fb4d755852cbac6f22fc6ed1
HermutHermit <milanahermut@gmail.com>
2024-09-16 01:04:04 +0300

    Практическое задание по теме "Переменные".

---
+++ peremennyye.py
@@ -0,0 +1,5 @@
+homework = 12
+time_homework = 1.5
+title = "Python"
+task_time = time_homework / homework
+print ("Курс:" ,title,", всего задач:" ,homework, ", затрачено часов:", time_homework, ", среднее время выполнения:",  task_time, "часа.")
