51ab7ce0027acf1f98233949625491aa202540d2
HermutHermit <milanahermut@gmail.com>
2024-09-22 14:44:41 +0300

    Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи"

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = 18, 12, 98 , "Svetlana", True, [12,15,27]
+print (immutable_var)
+# immutable_var [0] = 56
+#print (immutable_var)
+# Элементы кортежа нельзя изменить, потому что это неизменяемый объект
+
+mutable_list = [25, 74, "onion" , "sheese"]
+print (mutable_list)
+mutable_list [0] = 69
+mutable_list [2] = 'tea'
+print (mutable_list)
