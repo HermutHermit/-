9864ff60b3052488dd60f67c172e62200ab7311b
HermutHermit <milanahermut@gmail.com>
2024-09-24 00:52:45 +0300

    Домашняя работа по уроку "Условная конструкция. Операторы if, elif, else."

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = 952
+second = 465
+third = 159
+
+if  first == second and second == third:
+    print(3)
+elif first == second or second == third or third == first:
+    print (2)
+else:
+    print (0)
